a4933968b1144032c9907f9c1ffb4b59503d1289
ktKongTong <44502608+ktKongTong@users.noreply.github.com>
2023-03-31 12:28:10 +0800

    :sparkles: add cloudflare web analytics

---
+++ docs/.vuepress/plugins-cloudflare-analytics/client/composables/index.ts
@@ -0,0 +1,38 @@
+import type { CloudflareAnalyticsPluginOptions } from '../../shared'
+
+declare const dataLayer: any[]
+declare const gtag: (...args: any[]) => void
+
+declare global {
+    interface Window {
+        dataLayer?: typeof dataLayer
+        gtag?: typeof gtag
+    }
+}
+
+export const useCloudflareAnalytics = (
+    options: CloudflareAnalyticsPluginOptions
+): void => {
+    // avoid duplicated import
+    if (window.dataLayer && window.gtag) {
+        return
+    }
+    const gtagScript = document.createElement('script')
+    gtagScript.src = `https://static.cloudflareinsights.com/beacon.min.js`
+    gtagScript.defer = true
+    gtagScript.setAttribute("data-cf-beacon",`{"token"${options.token}}`)
+    console.log(gtagScript)
+    document.head.appendChild(gtagScript)
+    window.dataLayer = window.dataLayer || []
+    window.gtag = function () {
+        dataLayer.push(arguments)
+    }
+
+    gtag('js', new Date())
+
+    if (options.debug) {
+        gtag('config', options.token, { debug_mode: true })
+    } else {
+        gtag('config', options.token)
+    }
+}
