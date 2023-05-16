27276e212e55cf6f7c9e2fdae3aad83e27997e0d
ktKongTong <44502608+ktKongTong@users.noreply.github.com>
2023-05-16 08:16:41 +0800

    :memo: update config

--- docs/.vuepress/link/mdlink.ts
+++ docs/.vuepress/link/mdlink.ts
@@ -62,7 +62,7 @@ export const replaceLink =  function (link, env, token, htmlToken) {
 
 const replaceImageLink = (link, env, token, htmlToken):string =>{
     if (!link.startsWith("/")){
-        return "/"+link
+        return "https://cdn.jsdelivr.net/gh/ktKongTong/btnews@master/docs/.vuepress/public/"+link
     }
     return link
 }
