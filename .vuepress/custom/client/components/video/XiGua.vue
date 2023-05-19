0b2b723a2f2011d7c31c03a5bc478329cd684d9c
ktKongTong <44502608+ktKongTong@users.noreply.github.com>
2023-05-19 13:05:13 +0800

    support XiGua video

---
+++ .vuepress/custom/client/components/video/XiGua.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+       const { id, start } =  defineProps({
+        id: {
+            type: String,
+            default: ""
+        },
+        start:{
+            type: String,
+            default: "0"
+        },
+    })
+    const src = `https://www.ixigua.com/iframe/${id}?autoplay=0&startTime=${start}`
+</script>
+<template>
+    <iframe 
+    :src="src"
+    frameborder="0" allowfullscreen 
+     referrerpolicy="unsafe-url">
+    </iframe>
+</template>
