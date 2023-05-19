32662003e30727a3ba35bd57adbe3420e8b229af
ktKongTong <44502608+ktKongTong@users.noreply.github.com>
2023-05-19 13:05:13 +0800

    support XiGua video

--- .vuepress/custom/client/components/video/VideoService.vue
+++ .vuepress/custom/client/components/video/VideoService.vue
@@ -1,10 +1,24 @@
 <script setup lang="ts">
 import Youtube from "./Youtube.vue";
 import BiliBili from "./BiliBili.vue";
-import TabGroup from "./TabGroup.vue";
-import TabPanel from "./TabPanel.vue";
+import XiGua from "./XiGua.vue";
+import TabGroup from "./tab/TabGroup.vue";
+import TabPanel from "./tab/TabPanel.vue";
 import { type PropType } from 'vue';
-type Provider ="Bilibili" | "Youtube"
+
+type Provider ="Bilibili" | "Youtube" | "bilibili" | "youtube"
+type Video = {
+    provider: Provider,
+    id: string
+}
+const providerMap = {
+    "Bilibili": BiliBili,
+    "bilibili": BiliBili,
+    "Youtube": Youtube,
+    "youtube": Youtube,
+    "XiGua": XiGua,
+    "xigua": XiGua,
+}
 type VideoProvider = Array<Provider> | Provider
 
 const { provider, videoId } = defineProps<{
@@ -17,13 +31,6 @@ const { provider, videoId } = defineProps<{
             required: true
     },
 }>()
-
-let videoProviders: Array<Provider> = []
-if (typeof provider === "string") {
-    videoProviders = [provider]
-}else{
-    videoProviders = provider as unknown as Array<Provider>
-}
 let videoIds:Array<string> = []
 if (typeof videoId === "string") {
     videoIds = [videoId]
@@ -31,35 +38,40 @@ if (typeof videoId === "string") {
     videoIds = videoId as unknown  as Array<string>
 }
 
-const videoIdMap = new Map<Provider, string>()
-videoProviders.forEach((provider, index) => {
-    videoIdMap.set(provider, videoIds[index])
-})
+let videoProviders: Array<Provider> = []
+if (typeof provider === "string") {
+    videoProviders = [provider]
+}else{
+    videoProviders = (provider as unknown as Array<Provider>)
+}
 
-const getComponent = (provider: Provider) => {
-    switch (provider) {
-        case "Bilibili":
-            return BiliBili
-        case "Youtube":
-            return Youtube
-        default:
-            return BiliBili
+const videoIdMap:Video[] = []
+videoProviders.forEach((provider, index) => {
+    if (!Object.keys(providerMap).includes(provider)) {
+        return
     }
+    videoIdMap.push({
+        provider: provider, 
+        id: videoIds[index]
+    })
+})
+const getComponent = (provider: Provider) => {
+    return providerMap[provider]
 }
 </script>
 
 <template>
     <div class="player-wrapper" id="player">
         <ClientOnly>
-            <TabGroup :defaultKey="videoProviders[0]" v-if="videoProviders.length > 1">
-                    <TabPanel :label="provider" :actKey="provider" :key="index"   v-for="(provider, index)  in videoProviders">
+            <TabGroup :defaultKey="0" v-if="videoIdMap.length > 1">
+                    <TabPanel :label="provider" :actKey="index" :key="index"   v-for="({provider,id}, index)  in videoIdMap">
                         <div style="position: relative; padding: 30% 45%;">
-                            <component :is="getComponent(provider)" :id="videoIdMap.get(provider)" start="0" style="position: absolute; width: 100%; height: 100%; left: 0; top: 0;border-radius: 10px;" />
+                            <component :is="providerMap[provider]" :id="id" start="0" style="position: absolute; width: 100%; height: 100%; left: 0; top: 0;border-radius: 10px;" />
                         </div>
                         </TabPanel>
              </TabGroup>
-             <div style="position: relative; padding: 30% 45%;" v-else>
-                <component :is="getComponent(videoProviders[0])" :id="videoIdMap.get(videoProviders[0])" start="0" style="position: absolute; width: 100%; height: 100%; left: 0; top: 0;border-radius: 10px;" />
+             <div style="position: relative; padding: 30% 45%;" v-else-if="videoIdMap.length == 1">
+                <component :is="getComponent(videoIdMap[0].provider)" :id="videoIdMap[0].id" start="0" style="position: absolute; width: 100%; height: 100%; left: 0; top: 0;border-radius: 10px;" />
             </div>
         </ClientOnly>
     </div>
