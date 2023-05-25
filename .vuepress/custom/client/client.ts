48ef1392b570cb7966e283767a87ec14f8f517ca
ktKongTong <44502608+ktKongTong@users.noreply.github.com>
2023-05-25 12:30:03 +0800

    :arrow_up: upgrade deps & use comment

--- .vuepress/custom/client/client.ts
+++ .vuepress/custom/client/client.ts
@@ -1,13 +1,11 @@
 import { defineClientConfig } from '@vuepress/client'
 import HomeCardList from './components/HomeCardList.vue'
 import VideoService from './components/video/VideoService.vue'
-import CommentService from './components/waline/CommentService.vue'
 
 
 export default defineClientConfig({
   enhance({ app, router, siteData }) {
     app.component('HomeCardList', HomeCardList)
     app.component('VideoService', VideoService)
-    app.component('CommentService', CommentService)
   },
 })
