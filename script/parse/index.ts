47ada83ec320334a731abc2642a12065e6828769
ktKongTong <44502608+ktKongTong@users.noreply.github.com>
2024-04-15 19:50:10 +0800

    add fetch script

---
+++ script/parse/index.ts
@@ -0,0 +1,13 @@
+import { BedtimeNewsFrontmatter } from "../interfaces";
+import * as cheerio from 'cheerio'
+import { extractItem } from "./extractor";
+import CheerioRoot = cheerio.Root
+export default async function parseHTML($:CheerioRoot, frontmatter: BedtimeNewsFrontmatter): Promise<string> {
+  const content = $("#js_content")
+  const children = content.children()
+  let mdContent = ""
+  for (let child of children) {
+    mdContent += await extractItem(frontmatter,child)
+  }
+  return mdContent
+}
