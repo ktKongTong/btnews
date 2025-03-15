9118f7e6de867a6e9b51afc970b2d083a6ec4049
ktKongTong <kt@ktlab.io>
2025-03-15 10:46:21 +0000

    ci: add title-parse shell script

---
+++ .github/scripts/title-parse.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+function format_range() {
+  local index=$1
+  formatted_index=$(printf "%04d" "$1")
+  local range_start=$(((index / 100) * 100))
+  local range_end=$((range_start + 99))
+  formatted_range=$(printf "%04d_%04d" "$range_start" "$range_end")
+}
+
+REGEX="^【([^0-9]+)\s?([0-9]+)期?】.+$"
+cat $1 | jq -r '.title' | while read -r title; do
+    if [[ $title =~ $REGEX ]];
+        then
+        category="${BASH_REMATCH[1]}"
+        index="${BASH_REMATCH[2]}"
+        # 映射 category
+        case "$category" in
+          "讲点黑话") mapped_category="commercial";;
+          "睡前消息") mapped_category="btnews";;
+          "高见") mapped_category="opinion";;
+          *) mapped_category="unknown"
+            ;;
+        esac
+
+        # 格式化 index 为4位数
+        formatted_index=$(printf "%04d" "$1")
+        format_range "$index"
+        path="docs/btnews/${mapped_category}/${formatted_range}"
+        filepath="docs/btnews/${mapped_category}/${formatted_range}/${mapped_category}_${formatted_index}.md"
+        echo "category=$mapped_category"
+        echo "index=$formatted_index"
+        echo "path=$path"
+        echo "filepath=$filepath"
+        echo "category=$mapped_category" >> "$GITHUB_OUTPUT"
+        echo "index=$formatted_index" >> "$GITHUB_OUTPUT"
+        echo "path=$path" >> "$GITHUB_OUTPUT"
+        echo "filepath=$filepath" >> "$GITHUB_OUTPUT"
+    else
+        echo "Title format not matched: $title"
+        exit 1
+    fi
+done
