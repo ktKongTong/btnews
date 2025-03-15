3fc5e7e8257e7955f74dfc7ad299375f54f508c5
ktKongTong <kt@ktlab.io>
2025-03-15 12:00:12 +0000

    ci: update set-input.sh

--- .github/scripts/set-input.sh
+++ .github/scripts/set-input.sh
@@ -1,19 +1,27 @@
+function setInputByCategory() {
+        case $1 in
+            "btnews") 
+                keyword="睡前消息"
+                mid="316568752" ;;
+            "opinion")
+                keyword="高见"
+                mid="59104725" ;;
+            "commercial")
+                keyword="讲点黑话"
+                mid="64219557" ;;
+        esac
+        echo "mid=$mid" >> "$GITHUB_OUTPUT"
+        echo "keyword=$keyword" >> "$GITHUB_OUTPUT"    
+}
 
 if [ -z "$bvid" ]; then
     if [ -z "$category" ]; then
         weekday=$(date +%w)
         case $weekday in
-            0|2|5) 
-                keyword="睡前消息"
-                mid="316568752" ;;
-            1|4)
-                keyword="高见"
-                mid="59104725" ;;
-            3|6)
-                keyword="讲点黑话"
-                mid="64219557" ;;
+            0|2|5) category='btnews' ;;
+            1|4) category='opinion' ;;
+            3|6) category='commercial' ;;
         esac
-        echo "mid=$mid" >> "$GITHUB_OUTPUT"
-        echo "keyword=$keyword" >> "$GITHUB_OUTPUT"
     fi
+    setInputByCategory "$category"
 fi
